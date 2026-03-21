65b7c0e801ec08973026e8aa548bc61413ceb997
Nehuel Armenanzas <74217788+uncookednehuel@users.noreply.github.com>
2026-03-21 14:19:23 -0400

    Removed annoying license headers and started cburn

--- src/Classes/Chain.ts
+++ src/Classes/Chain.ts
@@ -1,18 +1,3 @@
-/**
- * Copyright 2025 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *       http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 class Chain {
   pins: Array<DefinedPin>;
 
@@ -49,9 +34,14 @@ class Chain {
   }
 
   /**
-   * Writes the chain to the speadsheet
+   * Burns the chain to the speadsheet
    */
-  burnChain(): void {}
+  burnChain(): void {
+    this.pins.forEach((pin) => {
+      pin.tos = this.pins.map((p) => p.componentID).join(', ');
+      pin.toPins = this.pins.map((p) => p.pinAlphaNum).join(', ');
+    });
+  }
 
   /**
    * Returns string representation of the chain
